8546d230eff84e504ecb09f17c5faf72eb0a8c64
Paulo Cunha <paulocunha103@hotmail.com>
2023-01-25 14:31:18 +0000

    Design Prototype

--- main.py
+++ main.py
@@ -5,48 +5,59 @@ from PySide6 import QtCore, QtWidgets, QtGui
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowFlags( QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.WindowCloseButtonHint )
+        self.setWindowFlags( QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.X11BypassWindowManagerHint )
         self.font = QtGui.QFont()
 
-
-
         self.textTotal = QtWidgets.QLabel("Total: ")
-        self.counterTotal = QtWidgets.QLabel("0")
+        self.counterTotal = QtWidgets.QLabel("00")
         self.newButton= QtWidgets.QPushButton("Novo")
-        self.prompt = QtWidgets.QLineEdit("Nome")
 
         # Storage
         self.labels = [self.textTotal, self.counterTotal]
-        self.buttons = []
+        self.buttons = [self.newButton]
+        self.lines = []
 
         self.layout = QtWidgets.QFormLayout(self)
         self.layout.addRow(self.textTotal, self.counterTotal)
-        self.layout.addRow(self.prompt, self.newButton)
-        
+        self.layout.addRow(self.newButton)
+        self.magic()
 
         self.resizeEvent(self)
         self.newButton.clicked.connect(self.magic)
-        self.newButton.setFont(self.font)
+        
 
     @QtCore.Slot()
     def magic(self):
-        button = QtWidgets.QPushButton(self.prompt.text())
-        self.buttons.append(button)
+        line = QtWidgets.QLineEdit("Nome")
+        self.lines.append(line)
 
-        text = QtWidgets.QLabel("0")
+        text = QtWidgets.QLabel("00")
         self.labels.append(text)
 
-        self.layout.addRow(button, text)
+        self.layout.addRow(line, text)
         self.resizeEvent(self)
 
     def resizeEvent(self, event):
         width = self.frameGeometry().width()
         height = self.frameGeometry().height()
-        fontsize = int((width + height)/24)
-        self.font.setPointSize(fontsize)
+
+        # Control Sizes here
+        labelSize = int((width + height)/24)
+        buttonSize = int((width + height)/32)
+        lineSize = int((width + height)/32)
+
+        self.font.setPointSize(labelSize)
         for label in self.labels:
             label.setAlignment(QtCore.Qt.AlignCenter)
             label.setFont(self.font)
+
+        self.font.setPointSize(buttonSize)    
+        for button in self.buttons:
+            button.setFont(self.font)
+
+        self.font.setPointSize(lineSize)    
+        for line in self.lines:
+            line.setFont(self.font)
 
 def main():
     # Start Application
